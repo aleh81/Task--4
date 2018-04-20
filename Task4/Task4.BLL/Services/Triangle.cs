0aa5b989033670479eb2d65738877b2a4a359979
Oleg <User@DESKTOP-2L28T8V>
2018-04-20 20:35:10 +0300

    Add Perimetr and HalfPerimetr method

---
+++ Task4/Task4.BLL/Services/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Task4.BLL.Services
+{
+	public class Triangle
+	{
+		private const string ExMessage = "Error: The sides of a triangle can not be <= 0";
+		private readonly double _a;
+		private readonly double _b;
+		private readonly double _c;
+
+		public Triangle(double a, double b, double c)
+		{
+			if ((a <= 0) || (b <= 0) || (c <=0))
+			{
+				throw new ArgumentException(ExMessage);
+			}
+			else
+			{
+				this._a = a;
+				this._b = b;
+				this._c = c;
+			}
+		}
+
+		public double Perimetr() => _a + _b + _c;
+
+		public double HalfPerimetr() => Perimetr() / 2;
+
+	}
+}
---
+++ Task4/Task4.BLLTests/Services/TriangleTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task4.BLL.Services;
+
+namespace Task4.BLLTests.Services
+{
+	[TestClass()]
+	public class TriangleTests
+	{
+		[TestMethod()]
+		public void ConstructorTestNegativeArguments()
+		{
+			try
+			{
+				var tiangle = new Triangle(0, -1, 2);
+			}
+			catch (ArgumentException e)
+			{
+				Assert.AreEqual("System.ArgumentException", e.GetType().ToString());
+			}
+
+			try
+			{
+				var trianglePositive = new Triangle(5, 1, 9);
+			}
+			catch (Exception e)
+			{
+				Assert.AreEqual(e, null);
+			}
+		}
+
+		[TestMethod()]
+		public void PerimetrTest()
+		{
+			var tringle1 = new Triangle(9, 5, 7);
+			var triangle2 = new Triangle(7.01, 22.657, 34.05);
+
+			Assert.AreEqual(21, tringle1.Perimetr());
+			Assert.AreEqual(63.717, triangle2.Perimetr());
+		}
+
+		[TestMethod()]
+		public void HalfPerimetrTest()
+		{
+			var triangle1 = new Triangle(4, 8, 6);
+			var triangle2 = new Triangle(9.64, 3.002, 5.972);
+
+			Assert.AreEqual(9, triangle1.HalfPerimetr());
+			Assert.AreEqual(9.307, triangle2.HalfPerimetr());
+		}
+	}
+}
