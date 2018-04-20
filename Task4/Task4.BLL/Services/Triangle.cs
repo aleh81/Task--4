29578fe49d0dc1c4b445f656a88434e915465ac9
Oleg <User@DESKTOP-2L28T8V>
2018-04-21 01:00:33 +0300

    Add Exsist method

--- Task4/Task4.BLL/Services/Triangle.cs
+++ Task4/Task4.BLL/Services/Triangle.cs
@@ -10,28 +10,48 @@ namespace Task4.BLL.Services
 {
 	public class Triangle
 	{
-		private const string ExMessage = "Error: The sides of a triangle can not be <= 0";
-		private readonly double _a;
-		private readonly double _b;
-		private readonly double _c;
+		private double _a;
+		private double _b;
+		private double _c;
+
+		public Triangle()
+		{
+			_a = 0;
+			_b = 0;
+			_c = 0;
+		}
 
 		public Triangle(double a, double b, double c)
 		{
-			if ((a <= 0) || (b <= 0) || (c <=0))
-			{
-				throw new ArgumentException(ExMessage);
-			}
-			else
-			{
-				this._a = a;
-				this._b = b;
-				this._c = c;
-			}
+				_a = Math.Abs(a);
+				_b = Math.Abs(b);
+				_c = Math.Abs(c);
 		}
 
-		public double Perimetr() => _a + _b + _c;
+		public bool Exsist => (!(_a <= 0)) && (!(_b <= 0)) && (!(_c <= 0));
 
-		public double HalfPerimetr() => Perimetr() / 2;
+		public double Perimetr => _a + _b + _c;
 
+		public double HalfPerimetr => Perimetr / 2;
+
+		public double Area()
+		{
+			var halfPerimetr = HalfPerimetr;
+
+			var p = (halfPerimetr - _a) * (halfPerimetr - _b) * (halfPerimetr - _c);
+
+			var s = halfPerimetr * p;
+
+			return Math.Sqrt(s);
+		}
+
+		public override string ToString()
+		{
+			return $"A ={_a: 0.00} cm; B ={_b: 0.00} cm; C ={_c: 0.00} cm;";
+		}
+
+		public string GetPerimetr => $"The perimetr of the triangle = {Perimetr: 0.00} cm";
+
+		public string GetArea => $"The area of the triangle = {Area(): 0.00} cm squared";
 	}
 }
--- Task4/Task4.UI/Program.cs
+++ Task4/Task4.UI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task4.BLL.Services;
 
 namespace Task4.UI
 {
@@ -10,6 +11,31 @@ namespace Task4.UI
 	{
 		static void Main(string[] args)
 		{
+			var triangle = new Triangle(4.95, 5.244, 3.11);
+			var triangle2 = new Triangle();
+
+			if (triangle.Exsist)
+			{
+				Console.WriteLine($"Sides of triangle: {triangle}");
+				Console.WriteLine(triangle.GetPerimetr);
+				Console.WriteLine(triangle.GetArea);
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.WriteLine("Triangle is not defined");
+			}
+
+			if (triangle2.Exsist)
+			{
+				Console.WriteLine(triangle2);
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.WriteLine("Triangle is not defined");
+			}
+			Console.ReadKey();
 		}
 	}
 }
